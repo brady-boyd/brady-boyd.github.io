d6f5620f0a980701841117e2907e7441e845183b
brady-wgu <bboy102@wgu.edu>
2023-07-18 18:04:06 -0500

    Initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 69,
         "density": {
           "enable": true,
           "value_area": 800
